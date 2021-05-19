202bd3106ee886819fdd0f550318f9d18120fa37
Antonis Christofides <antonis@antonischristofides.com>
2021-05-19 12:42:39 +0300

    Improve variable names

--- easter.js
+++ easter.js
@@ -8,29 +8,38 @@ function monthDays(month) {
   throw Error("Something's wrong");
 }
 
+const DAYS_IN_12_MOONS = 12 * 29.5;
+const DIFF_BETWEEN_12_MOONS_AND_ONE_YEAR = 365 - DAYS_IN_12_MOONS;
+
 function getEasterDate(year) {
   let month;
   let dayOfMonth;
 
-  const a = year % 19;
+  const yearNumberInMetonicCycle = year % 19;
+  const century = Math.floor(year / 100);
+  const p = Math.floor((13 + 8 * century) / 25);
+  const quadricentennial = Math.floor(century / 4);
+  const M = (15 - p + century - quadricentennial) % 30;
+  const daysFromEquinoxToFullMoon = (
+    ((30 - DIFF_BETWEEN_12_MOONS_AND_ONE_YEAR) * yearNumberInMetonicCycle + M) % 30
+  );
+
   const b = year % 4;
   const c = year % 7;
-  const k = Math.floor(year / 100);
-  const p = Math.floor((13 + 8 * k) / 25);
-  const q = Math.floor(k / 4);
-  const M = (15 - p + k - q) % 30;
-  const N = (4 + k - q) % 7;
-  const d = (19 * a + M) % 30;
-  const e = (2 * b + 4 * c + 6 * d + N) % 7;
-  if (d === 29 && e === 6) {
+  const N = (4 + century - quadricentennial) % 7;
+  const daysFromFullMoonToSunday = (2 * b + 4 * c + 6 * daysFromEquinoxToFullMoon + N) % 7;
+
+  if (daysFromEquinoxToFullMoon === 29 && daysFromFullMoonToSunday === 6) {
     month = 4;
     dayOfMonth = 19;
-  } else if (d === 28 && e === 6 && (11 * M + 11) % 30 < 19) {
+  } else if (
+    daysFromEquinoxToFullMoon === 28 && daysFromFullMoonToSunday === 6 && (11 * M + 11) % 30 < 19
+  ) {
     month = 4;
     dayOfMonth = 18;
   } else {
     month = 3;
-    dayOfMonth = 22 + d + e;
+    dayOfMonth = 22 + daysFromEquinoxToFullMoon + daysFromFullMoonToSunday;
     if (dayOfMonth > 31) {
       month = 4;
       dayOfMonth -= 31;
