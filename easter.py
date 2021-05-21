1ea95ca583370f1ae59e6a2b58490ad641e52a9b
Antonis Christofides <antonis@antonischristofides.com>
2021-05-21 14:19:08 +0300

    Refactor

--- easter.py
+++ easter.py
@@ -1,44 +1,65 @@
+import datetime as dt
+
 DAYS_IN_12_MOONS = 12 * 29.5
 DIFF_BETWEEN_12_MOONS_AND_ONE_YEAR = 365 - DAYS_IN_12_MOONS
 
 
-def get_easter_date(year):
-    year_number_in_metonic_cycle = year % 19
-    century = year // 100
-    p = (13 + 8 * century) // 25
-    quadricentennial = century // 4
-    M = (15 - p + century - quadricentennial) % 30
-    days_from_equinox_to_full_moon = (
-        (30 - DIFF_BETWEEN_12_MOONS_AND_ONE_YEAR) * year_number_in_metonic_cycle + M
-    ) % 30
+class EasterDate:
+    def __init__(self, year):
+        self.year = year
+        self.calculate()
 
-    b = year % 4
-    c = year % 7
-    N = (4 + century - quadricentennial) % 7
-    days_from_full_moon_to_sunday = (
-        2 * b + 4 * c + 6 * days_from_equinox_to_full_moon + N
-    ) % 7
+    def calculate(self):
+        self.find_days_from_equinox_to_full_moon()
+        self.find_days_from_full_moon_to_sunday()
+        self.find_easter_sunday()
 
-    exception1 = (
-        days_from_equinox_to_full_moon == 29 and days_from_full_moon_to_sunday == 6
-    )
-    exception2 = (
-        days_from_equinox_to_full_moon == 28
-        and days_from_full_moon_to_sunday == 6
-        and (11 * M + 11) % 30 < 19
-    )
-    if exception1:
-        month = 4
-        day_of_month = 19
-    elif exception2:
-        month = 4
-        day_of_month = 18
-    else:
-        month = 3
-        day_of_month = (
-            22 + days_from_equinox_to_full_moon + days_from_full_moon_to_sunday
+    def find_days_from_equinox_to_full_moon(self):
+        year_number_in_metonic_cycle = self.year % 19
+        self.century = self.year // 100
+        self.quadricentennial = self.century // 4
+        p = (13 + 8 * self.century) // 25
+        self.M = (15 - p + self.century - self.quadricentennial) % 30
+        self.days_from_equinox_to_full_moon = (
+            (30 - DIFF_BETWEEN_12_MOONS_AND_ONE_YEAR) * year_number_in_metonic_cycle
+            + self.M
+        ) % 30
+
+    def find_days_from_full_moon_to_sunday(self):
+        b = self.year % 4
+        c = self.year % 7
+        N = (4 + self.century - self.quadricentennial) % 7
+        self.days_from_full_moon_to_sunday = (
+            2 * b + 4 * c + 6 * self.days_from_equinox_to_full_moon + N
+        ) % 7
+
+    def find_easter_sunday(self):
+        self.easter_sunday = dt.date(self.year, 3, 22) + dt.timedelta(
+            self.days_from_equinox_to_full_moon + self.days_from_full_moon_to_sunday
         )
-        if day_of_month > 31:
-            month = 4
-            day_of_month -= 31
-    return [month, day_of_month]
+        self.deal_with_exceptions()
+
+    def deal_with_exceptions(self):
+        self.historically_forbid_easter_from_falling_on_26_april()
+        self.ensure_full_moons_dont_occur_on_same_date_twice_in_a_metonic_cycle()
+
+    def ensure_full_moons_dont_occur_on_same_date_twice_in_a_metonic_cycle(self):
+        this_would_be_the_second_full_moon_on_18_april_in_a_metonic_cycle = (
+            self.days_from_equinox_to_full_moon == 28 and (11 * self.M + 11) % 30 < 19
+        )
+        if this_would_be_the_second_full_moon_on_18_april_in_a_metonic_cycle:
+            if self.days_from_full_moon_to_sunday == 6:
+                self.easter_sunday = dt.date(self.year, 4, 18)
+
+    def historically_forbid_easter_from_falling_on_26_april(self):
+        easter_would_normally_fall_on_26_april = (
+            self.days_from_equinox_to_full_moon == 29
+            and self.days_from_full_moon_to_sunday == 6
+        )
+        if easter_would_normally_fall_on_26_april:
+            self.easter_sunday = dt.date(self.year, 4, 19)
+
+
+def get_easter_date(year):
+    easter_date = EasterDate(year)
+    return easter_date.easter_sunday
--- test_easter.py
+++ test_easter.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import unittest
 
 from easter import get_easter_date
@@ -5,25 +6,25 @@ from easter import get_easter_date
 
 class GetEasterDateTestCase(unittest.TestCase):
     def test_earliest_possible_date(self):
-        self.assertEqual(get_easter_date(1598), [3, 22])
+        self.assertEqual(get_easter_date(1598), dt.date(1598, 3, 22))
 
     def test_latest_possible_date(self):
-        self.assertEqual(get_easter_date(1666), [4, 25])
+        self.assertEqual(get_easter_date(1666), dt.date(1666, 4, 25))
 
     def test_exception1(self):
-        self.assertEqual(get_easter_date(1609), [4, 19])
+        self.assertEqual(get_easter_date(1609), dt.date(1609, 4, 19))
 
     def test_non_special_19_april(self):
-        self.assertEqual(get_easter_date(1615), [4, 19])
+        self.assertEqual(get_easter_date(1615), dt.date(1615, 4, 19))
 
     def test_exception2(self):
-        self.assertEqual(get_easter_date(1954), [4, 18])
+        self.assertEqual(get_easter_date(1954), dt.date(1954, 4, 18))
 
     def test_non_special_18_april(self):
-        self.assertEqual(get_easter_date(1965), [4, 18])
+        self.assertEqual(get_easter_date(1965), dt.date(1965, 4, 18))
 
     def test_correct_p(self):
-        self.assertEqual(get_easter_date(4200), [4, 20])
+        self.assertEqual(get_easter_date(4200), dt.date(4200, 4, 20))
 
 
 if __name__ == '__main__':
